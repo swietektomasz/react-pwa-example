e56c34b15def46f095fbbecb3c27bcd3d4734fef
swietektomasz <swietek.tomasz@protonmail.com>
2019-11-18 17:51:29 +0100

    Firebase based auth

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from 'react'
-import './App.css'
-import MainView from './containers/main'
+import * as React from 'react'
+import withFirebaseAuth from 'react-with-firebase-auth'
+import * as firebase from 'firebase/app'
 
-function App() {
-  return <MainView />
+import 'firebase/auth'
+import firebaseConfig from './firebaseConfig'
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+
+const firebaseAppAuth = firebaseApp.auth()
+
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
 }
 
-export default App
+const App = ({ signInWithGoogle, signOut, user }) => (
+  <div className="App">
+    <header className="App-header">
+      {user ? <p>Hello, {user.displayName}</p> : <p>Please sign in.</p>}
+      {user ? (
+        <button onClick={signOut}>Sign out</button>
+      ) : (
+        <button onClick={signInWithGoogle}>Sign in with Google</button>
+      )}
+    </header>
+  </div>
+)
+
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App)
---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAVG3yWj2Fn93o7dZtwY3jMAcbEiN2hfIk',
+  authDomain: 'react-pwa-example-1efe2.firebaseapp.com',
+  databaseURL: 'https://react-pwa-example-1efe2.firebaseio.com',
+  projectId: 'react-pwa-example-1efe2',
+  storageBucket: 'react-pwa-example-1efe2.appspot.com',
+  messagingSenderId: '906901561310',
+  appId: '1:906901561310:web:41b1a3eef4369bcdef074f',
+}
+
+export default firebaseConfig
