fc4ecde169c3b9bbe138811b44514e398ad6cc7b
swietektomasz <swietek.tomasz@protonmail.com>
2019-11-24 21:10:00 +0100

    Some more styling

--- src/components/AnimeCard/index.js
+++ src/components/AnimeCard/index.js
@@ -2,20 +2,26 @@ import React from 'react'
 
 import './anime-card.css'
 
-const AnimeCard = ({ anime }) => (
-  <div className='anime-card'>
-    <ol>
-      <li>Title: {anime.title.userPreferred}</li>
-      <li>Average score: {anime.averageScore}</li>
-      <li>Started: {`${anime.startDate.day}.${anime.startDate.month}.${anime.startDate.year}`}</li>
-      <li>
-        Ended:{' '}
-        {anime.endDate.day
-          ? `${anime.endDate.day}.${anime.endDate.month}.${anime.endDate.year}`
-          : 'Not ended'}
-      </li>
-    </ol>
-  </div>
-)
+const AnimeCard = ({ anime }) => {
+  console.log(anime.coverImage.large)
+  return (
+    <div className='anime-card' style={{ backgroundColor: anime.coverImage.color }}>
+      <img src={anime.coverImage.large} alt='cover' />
+      <ol>
+        <li>Title: {anime.title.userPreferred}</li>
+        <li>Average score: {anime.averageScore}</li>
+        <li>
+          Started: {`${anime.startDate.day}.${anime.startDate.month}.${anime.startDate.year}`}
+        </li>
+        <li>
+          Ended:{' '}
+          {anime.endDate.day
+            ? `${anime.endDate.day}.${anime.endDate.month}.${anime.endDate.year}`
+            : 'Not ended'}
+        </li>
+      </ol>
+    </div>
+  )
+}
 
 export default AnimeCard
